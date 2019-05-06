7b492b5d933be7ea12d309403f1491344aeb18ae
k-kanada <kanada@pcl.cs.waseda.ac.jp>
2019-05-06 20:21:26 +0900

    add bucket func

--- colab_helpers.py
+++ colab_helpers.py
@@ -2,10 +2,12 @@ from google.colab import drive
 import subprocess
 import sys
 import importlib
+import tensorflow as tf
 
 def run_shell(cmd):
     res = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
     sys.stdout.write(res.stdout)
+    return res
 
 def mount_drive(path):
     drive.mount(path)
@@ -23,10 +25,10 @@ def register_git_ssh_key(ssh_path, email, user_name):
 def git(repo_path, command):
     run_shell("git -C {} {}".format(repo_path, command))
 
-def reload_from_str(modules, globals):
+# reload modules
+def reloads(modules, globals):
     if type(modules) == str:
-        str_modules = [x.strip() for x in modules.split(',')]
-        modules = [importlib.import_module(x) for x in str_modules]
+        modules = [x.strip() for x in modules.split(',')]
     for module in modules:
         reload(module, globals)
 
@@ -38,3 +40,25 @@ def reload(module, globals):
     funcs = [func for func in dir(module) if not func.startswith('__')]
     for func_name in funcs:
         globals()[func_name] = module.__dict__[func_name]
+
+# save/load from buckets
+def is_exist_in_bucket(*fnames):
+    fname = os.path.join(*fnames)
+    res = run_shell("gsutil -q stat {}".format(fname))
+    return res.returncode == 0
+
+def save_to_bucket(bucket_dir, fname):
+  tf.gfile.MakeDirs(bucket_dir)
+  bucket_fname = os.path.join(bucket_dir, fname)
+  run_shell("gsutil cp {} {}".format(fname, bucket_fname))
+
+def load_from_bucket(bucket_dir, fname):
+  bucket_fname = os.path.join(bucket_dir, fname)
+  run_shell("gsutil cp {} {}".format(bucket_fname,fname))
+
+def load_or_execute(bucket_dir, fname, func, force_execution = False, *args, **kwargs):
+    if force_execution or not is_exist_in_bucket(bucket_dir,fname):
+        func(args, kwargs)
+        save_to_bucket(bucket_dir,fname)
+    else:
+        load_from_bucket(bucket_dir,fname)
