118ebcaccc6135defb28b8bf3cf49f4f59881468
k-kanada <kanada@pcl.cs.waseda.ac.jp>
2019-05-06 16:04:28 +0900

    rewrite run_shell

--- colab_helpers.py
+++ colab_helpers.py
@@ -1,13 +1,18 @@
 from google.colab import drive
+import subprocess
+import sys
+
+def run_shell(cmd):
+    res = subprocess.run(cmd, stdout=subprocess.PIPE)
+    sys.stdout.write(res.stdout)
 
 def mount_drive(path):
     drive.mount(path)
 
 def register_git_ssh_key(ssh_path, email, user_name):
-    ! rm - rf / root /.ssh /
-    ! cp -r {ssh_path} /root/.ssh || chmod 700 /root/.ssh
-    ! ssh-keyscan github.com >> /root/.ssh/known_hosts
-    ! chmod 644 /root/.ssh/known_hosts
-    ! git config --global user.email {email}
-    ! git config --global user.name {user_namee}
-
+    run_shell("rm - rf / root /.ssh /")
+    run_shell("cp -r {} /root/.ssh || chmod 700 /root/.ssh".format(ssh_path))
+    run_shell("ssh-keyscan github.com >> /root/.ssh/known_hosts")
+    run_shell("chmod 644 /root/.ssh/known_hosts")
+    run_shell("git config --global user.email {}".format(email))
+    run_shell("git config --global user.name {}".format(user_name))
