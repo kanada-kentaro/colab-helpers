c9ec4779b2f632aaf80c15efca9c1ef7df61d2bf
k-kanada <kanada@pcl.cs.waseda.ac.jp>
2019-05-06 16:17:16 +0900

    fix typo

--- colab_helpers.py
+++ colab_helpers.py
@@ -10,9 +10,10 @@ def mount_drive(path):
     drive.mount(path)
 
 def register_git_ssh_key(ssh_path, email, user_name):
-    run_shell("rm - rf / root /.ssh /")
+    run_shell("rm -rf /root /.ssh/")
     run_shell("cp -r {} /root/.ssh || chmod 700 /root/.ssh".format(ssh_path))
     run_shell("ssh-keyscan github.com >> /root/.ssh/known_hosts")
     run_shell("chmod 644 /root/.ssh/known_hosts")
     run_shell("git config --global user.email {}".format(email))
     run_shell("git config --global user.name {}".format(user_name))
+
