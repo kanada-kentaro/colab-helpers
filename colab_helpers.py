b1934482e664b9a9297c8baf6ef19ef4b15e1383
k-kanada <kanada@pcl.cs.waseda.ac.jp>
2019-05-08 23:33:43 +0900

    add mpack loader

--- colab_helpers.py
+++ colab_helpers.py
@@ -89,3 +89,9 @@ def to_mpack(data, fname):
     packer = msgpack.Packer(default=m.encode)
     with open(fname, "wb") as file:
         file.write(packer.pack(data))
+
+def load_mpack(fname):
+    return msgpack.unpack(open(fname, "rb"), object_hook=m.decode)
+
+def mpack_loader(fname):
+    return msgpack.Unpacker(open(fname, "rb"), object_hook=m.decode)
